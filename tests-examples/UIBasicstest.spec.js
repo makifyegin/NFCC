c3db8bc78e52c1d4acb90215ada3a14f6409f49c
akif <mehmetakifyegin@icloud.com>
2023-01-13 20:39:48 +0000

    Initial Commit

---
+++ tests-examples/UIBasicstest.spec.js
@@ -0,0 +1,12 @@
+const {test} = require('@playwright/test');
+
+
+test('NFCC test', async ({browser})=>
+{
+    const context = browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://www.nationalfirechiefs.org.uk/");
+
+
+
+})
