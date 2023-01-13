ef4d95fab6b15e2f6e6956d645ea4fb471059ecf
akif <mehmetakifyegin@icloud.com>
2023-01-13 23:01:54 +0000

    Fix: mono fonts

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<span style="font-family: JetBrains Mono">
+<span style="font-family:./Font/ttf/JetBrainsMono-Regular.ttf">
 <div align="center">
   <a href="https://www.panlogic.co.uk/">
     <img src="images/panlogic-logo.png" alt="Logo">
--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,11 +1,6 @@
 const { test } = require('@playwright/test');
 
 
-test('NFCC test', async () => {
-    const context = browser.newContext();
-    const page = await context.newPage();
-    await page.goto("https://www.nationalfirechiefs.org.uk/");
-})
 
 
 test('NFCC test0', async ({ page }) => {
